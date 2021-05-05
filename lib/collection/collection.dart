296147c32d303979acc16aecad70898e76c39bd6
Faruk <faruk.ertuerk@visuellverstehen.de>
2021-05-05 22:23:43 +0200

    Collection Class Improved

--- lib/collection/collection.dart
+++ lib/collection/collection.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/foundation.dart';
+import '../cache/cache_handler.dart';
+import '../cache/cache_object.dart';
+import '../model/model.dart';
 
 abstract class Collection<T> {
   @protected
   final String _id;
 
+  @protected
   Map<String, dynamic> _models = {};
 
   Collection(this._id);
 
-  Future<void> setup() async {}
+  /// Load Model Data From Cache From Cache
+  @protected
+  Future<List<Map<String, dynamic>>> loadModelDataFromCache() async {
+    List<Map<String, dynamic>> result = [];
+    List<CacheObject> cache =
+        await CacheHandler.getCacheList("collection_" + _id);
+    for (CacheObject cacheObject in cache) {
+      result.add(cacheObject.data);
+    }
+    return result;
+  }
+
+  /// Save Model to Cache
+  @protected
+  Future<void> saveModel(Model model, {bool applyHiddenFields = true}) async {
+    await CacheHandler.saveCacheObjectToList(
+        "collection_" + _id,
+        CacheObject(model.getModelData(applyHiddenFields: applyHiddenFields)),
+        "id");
+  }
+
+  /// Initialize your Model Data Here
+  Future<void> setup();
 
   /// Returns Collection Id
   String getId() {
