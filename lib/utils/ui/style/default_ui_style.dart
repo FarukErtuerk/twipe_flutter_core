4376498ae58437d93832ef4b70a8b3675d887c2f
Faruk <faruk.ertuerk@visuellverstehen.de>
2021-05-06 02:11:03 +0200

    Default Ui Style Added

---
+++ lib/utils/ui/style/default_ui_style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class DefaultUiStyle {
+  /// Primary Color
+  Color get primary;
+
+  /// Secondary Color
+  Color get secondary;
+
+  /// Accent Color
+  Color get accent;
+}
