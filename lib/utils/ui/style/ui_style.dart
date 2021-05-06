dffecf3e1ddd8c9ba9ce4f27652ca7364fdae285
Faruk <faruk.ertuerk@visuellverstehen.de>
2021-05-06 20:29:29 +0200

    UiStyle Added

---
+++ lib/utils/ui/style/ui_style.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+/// This Class will help you designing your Application
+abstract class UiStyle {
+  /// Primary Color
+  Color get primary;
+
+  /// Secondary Color
+  Color get secondary;
+
+  /// Accent Color
+  Color get accent;
+
+  /// Darkest Color
+  Color get darkest;
+
+  /// Lightests Color
+  Color get lightest;
+
+  /// Base Color
+  Color get base;
+}
