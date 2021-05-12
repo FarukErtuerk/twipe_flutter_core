d93f9f715bf555bfa12a378d86e6936af3902eef
Faruk <faruk.ertuerk@visuellverstehen.de>
2021-05-12 21:16:53 +0200

    Text Style Added

--- lib/utils/ui/style/ui_color_style.dart
+++ lib/utils/ui/style/ui_color_style.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 /// This Class will help you designing your Application
-abstract class UiStyle {
+abstract class UIColorStyle {
   /// Primary Color
   Color get primary;
 
---
+++ lib/utils/ui/style/ui_text_style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class UITextStyle {
+  /// Primary Text Style.
+  TextStyle get primaryTextStyle;
+
+  /// Seconday Text Style.
+  TextStyle get secondaryTextStyle;
+
+  /// Accent Text Style.
+  TextStyle get accentTextStyle;
+}
