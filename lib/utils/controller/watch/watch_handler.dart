67765630e5f8ad4c99156de2a4ba999d077d542f
Faruk <faruk.ertuerk@visuellverstehen.de>
2021-05-29 02:14:22 +0200

    Quick FIx

--- lib/utils/controller/watch/watch_handler.dart
+++ lib/utils/controller/watch/watch_handler.dart
@@ -5,19 +5,19 @@ import 'watchable_change_result.dart';
 
 @protected
 class WatchHandler {
-  static Map<String, dynamic> watcher = {};
+  static Map<String, dynamic> _watcher = {};
 
   /// Register new Watcher
   static registerWatcher(dynamic watcher) {
-    watcher[watcher.watcherId] = watcher;
+    _watcher[watcher.watcherId] = watcher;
   }
 
   /// Trigger Watcher with `watcherId`
   static Future<WatchableChangeResult> trigger(String watcherId,
       WatchableChangeAction watchableChangeAction, dynamic data) async {
-    if (!watcher.containsKey(watcherId)) {
+    if (!_watcher.containsKey(watcherId)) {
       return WatchableChangeResult.error();
     }
-    return await watcher[watcherId].trigger(watchableChangeAction, data);
+    return await _watcher[watcherId].trigger(watchableChangeAction, data);
   }
 }
