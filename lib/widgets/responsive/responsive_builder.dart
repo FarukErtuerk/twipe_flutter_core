c6167073fd75369c15624980c474db6905f08d91
Faruk <faruk.ertuerk@visuellverstehen.de>
2021-06-21 19:04:40 +0200

    Fixed

--- lib/widgets/responsive/responsive_builder.dart
+++ lib/widgets/responsive/responsive_builder.dart
@@ -3,28 +3,33 @@ import 'package:twipe_flutter_core/utils/config/configuration.dart';
 import 'package:twipe_flutter_core/widgets/responsive/responsive_builder_mixin.dart';
 
 class ResponsiveBuilder extends StatelessWidget with ResponsiveBuilderMixin {
-  final WidgetBuilder mobileSmall;
-  final WidgetBuilder mobileMedium;
-  final WidgetBuilder mobileLarge;
-  final WidgetBuilder tablet;
-  final WidgetBuilder laptop;
-  final WidgetBuilder laptopLarge;
-  final WidgetBuilder fourK;
+  final WidgetBuilder? mobileSmall;
+  final WidgetBuilder? mobileMedium;
+  final WidgetBuilder? mobileLarge;
+  final WidgetBuilder? tablet;
+  final WidgetBuilder? laptop;
+  final WidgetBuilder? laptopLarge;
+  final WidgetBuilder? fourK;
   ResponsiveBuilder(this.mobileSmall, this.mobileMedium, this.mobileLarge,
       this.tablet, this.laptop, this.laptopLarge, this.fourK);
   @override
   Widget build(BuildContext context) {
-    if (Configuration.isScreen(context, fourK: true)) {
+    if (fourK != null && Configuration.isScreen(context, fourK: true)) {
       return buildFourK(context);
-    } else if (Configuration.isScreen(context, laptopLarge: true)) {
+    } else if (laptopLarge != null &&
+        Configuration.isScreen(context, laptopLarge: true)) {
       return buildLaptopLarge(context);
-    } else if (Configuration.isScreen(context, laptop: true)) {
+    } else if (laptop != null &&
+        Configuration.isScreen(context, laptop: true)) {
       return buildLaptop(context);
-    } else if (Configuration.isScreen(context, tablet: true)) {
+    } else if (tablet != null &&
+        Configuration.isScreen(context, tablet: true)) {
       return buildTablet(context);
-    } else if (Configuration.isScreen(context, mobileLarge: true)) {
+    } else if (mobileLarge != null &&
+        Configuration.isScreen(context, mobileLarge: true)) {
       return buildMobileLarge(context);
-    } else if (Configuration.isScreen(context, mobileMedium: true)) {
+    } else if (mobileMedium != null &&
+        Configuration.isScreen(context, mobileMedium: true)) {
       return buildMobileMedium(context);
     }
     return buildMobileSmall(context);
@@ -32,36 +37,36 @@ class ResponsiveBuilder extends StatelessWidget with ResponsiveBuilderMixin {
 
   @override
   Widget buildFourK(BuildContext context) {
-    return fourK(context);
+    return fourK!(context);
   }
 
   @override
   Widget buildLaptop(BuildContext context) {
-    return laptop(context);
+    return laptop!(context);
   }
 
   @override
   Widget buildLaptopLarge(BuildContext context) {
-    return laptopLarge(context);
+    return laptopLarge!(context);
   }
 
   @override
   Widget buildMobileLarge(BuildContext context) {
-    return mobileLarge(context);
+    return mobileLarge!(context);
   }
 
   @override
   Widget buildMobileMedium(BuildContext context) {
-    return mobileMedium(context);
+    return mobileMedium!(context);
   }
 
   @override
   Widget buildMobileSmall(BuildContext context) {
-    return mobileSmall(context);
+    return mobileSmall!(context);
   }
 
   @override
   Widget buildTablet(BuildContext context) {
-    return tablet(context);
+    return tablet!(context);
   }
 }
